70aef8a3a4330e904c285f99f4f55791f5be7782
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2022-06-30 17:59:32 +0300

    Second commit

---
+++ IJunior_HomeWork_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJunior_HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dmitry Semenov";
+            int number=-10;
+            uint age=29;
+            byte weight=98;
+            sbyte temperature =-20;
+            long longNumber=7459348583953985;
+            float speed=65.7f;
+            double numberPi = 3.1415926535;
+            char symbol='S';
+            bool isIJunior=true;
+        }
+    }
+}
